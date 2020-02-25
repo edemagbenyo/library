f11f1d260fbe9133b3a0ff9cc40cabf3c2e93627
Selma Datagirl <datagirlcmr@gmail.com>
2020-02-25 16:22:31 -0600

    changed the form display, add delete button on each book

--- main.js
+++ main.js
@@ -1,26 +1,38 @@
-const inputTitle = document.querySelector('#title');
-const inputAuthor = document.querySelector('#author');
-const inputPages = document.querySelector('#pages');
-const inputRead = document.querySelector('#read');
-const formBook = document.querySelector('form');
-const divBooks = document.querySelector('.books');
+const inputTitle = document.querySelector("#title");
+const inputAuthor = document.querySelector("#author");
+const inputPages = document.querySelector("#pages");
+const inputRead = document.querySelector("#read");
+const formBook = document.querySelector("form");
+const divBooks = document.querySelector(".books");
+const formButton = document.querySelector("#button")
 
-formBook.addEventListener('submit',(e)=>{
+formButton.addEventListener("click", () => {
+  formBook.style.display = "block"
+})
+
+formBook.style.display = "none"
+
+formBook.addEventListener("submit", e => {
   console.log(e);
   e.preventDefault();
   const title = inputTitle.value;
   const author = inputAuthor.value;
   const pages = inputPages.value;
   const read = inputRead.value;
-  addBookToLibrary(title,author,pages,read);
+  addBookToLibrary(title, author, pages, read);
 });
+
 let myLibrary = [
-  {title:'Business laws', author:'Brian Tracy', pages:900, read:false},
-  {title:'Rich dad, poor dad', author:'Robert Kiyo.', pages:239, read:true}
+  { title: "Business laws", author: "Brian Tracy", pages: 900, read: false },
+  {
+    title: "Rich dad, poor dad",
+    author: "Robert Kiyo.",
+    pages: 239,
+    read: true
+  }
 ];
 
-
-function Book(title,author,pages,read) {
+function Book(title, author, pages, read) {
   // the constructor...
   this.title = title;
   this.author = author;
@@ -30,36 +42,48 @@ function Book(title,author,pages,read) {
 
 function addBookToLibrary(...params) {
   // do stuff here
-  const book = new Book(params[0],params[1],params[2],params[3]);
+  const book = new Book(params[0], params[1], params[2], params[3]);
 
   myLibrary.push(book);
-  createBook(book)
-
+  const idBook = (myLibrary.length - 1);
+  createBook(book, idBook);
 }
 
-const render = ()=>{
-  myLibrary.forEach(book=>{
-    createBook(book)
-    // console.log("each",book)
-  }) 
+const render = () => {
+  myLibrary.forEach((book, bookId) => {
+    createBook(book, bookId);
+  });
+};
+
+function createBook(book, bookId) {
+  const div = document.createElement("div");
+  div.classList.add("card");
+
+  const pTitle = document.createElement("p");
+  const pAuthor = document.createElement("p");
+  const spanPages = document.createElement("p");
+  const spanRead = document.createElement("span");
+  const deleteBook = document.createElement("button");
+  deleteBook.classList.add('deletebuttton');
+  deleteBook.setAttribute("data-id", bookId);
+ 
+  pTitle.append(book.title);
+  pAuthor.append(book.author);
+  spanPages.append(book.pages);
+  deleteBook.innerHTML = "Delete"
+  spanRead.append(book.read == "on" ? "read" : "not read");
+
+  div.append(pTitle, pAuthor, spanPages, spanRead, deleteBook);
+  divBooks.appendChild(div);
 }
 
-function createBook(book){
-  const div = document.createElement('div');
-  div.classList.add('card')
+render();
 
-  const pTitle = document.createElement('p');
-  const pAuthor = document.createElement('p');
-  const spanPages = document.createElement('span');
-  const spanRead = document.createElement('span');
+const buttons = document.querySelectorAll(".deletebutton")
+buttons.forEach(button => {
+  button.addEventListener("click", bookDelete);
+})
 
-  pTitle.append(book.title)
-  pAuthor.append(book.author)
-  spanPages.append(book.pages)
-  spanRead.append(book.read=='on'?"read":"not read")
-
-  div.append(pTitle, pAuthor, spanPages,spanRead )
-  divBooks.appendChild(div)
+function bookDelete(event) {
+  console.log(event);
 }
-
-render()
